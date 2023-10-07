5045c567e6736b14ce70486daf2b232d0016097d
Ahmed Abdelhamid <ahmedabdelhamid887@gmail.com>
2023-10-07 20:27:03 +0300

    update akhbarak service

--- lib/models/item_body_model.dart
+++ lib/models/item_body_model.dart
@@ -1,7 +1,7 @@
 class ItemBodyModel {
-  final String itemImage;
-  final String itemtitle;
-  final String itemSubTitle;
+  final String? itemImage;
+  final String? itemtitle;
+  final String? itemSubTitle;
 
   const ItemBodyModel({
     required this.itemImage,
--- lib/services/akhbarak_service.dart
+++ lib/services/akhbarak_service.dart
@@ -2,23 +2,30 @@ import 'package:akhbark/models/item_body_model.dart';
 import 'package:dio/dio.dart';
 
 class AkhbarakService {
-   final Dio dio;
-   AkhbarakService(this.dio);
+  final Dio dio;
+  AkhbarakService(this.dio);
 
   // momken a3melaha zy el oula bardo w mast5dmsh el construtor
   //final Dio dio = Dio();
 
-  void getAkhbarak() async {
+  Future<List<ItemBodyModel>> getAkhbarak() async {
     Response response = await dio.get(
-        'https://newsapi.org/v2/top-headlines?country=eg&category=sports&apiKey=88e86952e1c746cea291f7f9e12aa4e6');
+        'https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=88e86952e1c746cea291f7f9e12aa4e6');
     Map<String, dynamic> jsonData = response.data;
-    //List<dynamic> articles = jsonData['articles'];
-    List<ItemBodyModel> articles = jsonData['articles'];
-    // for (var article in articles) {
-    //   print(article);
-    // }
-    //print(articles);
+    List<dynamic> articles = jsonData['articles'];
+    List<ItemBodyModel> articleList = [];
+    ItemBodyModel articleModel;
+    for (var article in articles) {
+      articleModel = ItemBodyModel(
+        itemImage: article['urlToImage'],
+        itemtitle: article['title'],
+        itemSubTitle: article['description'],
+      );
+
+      articleList.add(articleModel);
+    }
+
+    return articleList;
+    print(articleList);
   }
 }
-
-
